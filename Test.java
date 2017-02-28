2a6727593d71009296dc148284b7f2b3f81175ab
BHIMNATH <you@example.com>
2017-02-28 23:14:15 +0530

    A test java file

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test{
+	 static final int x=2;
+	public static void main(String[] args){
+				Test n=new Test();
+				System.out.println(n.x);
+	}
+} 
+//y=y+x;
+			//y=y+2;
+			/*if(y>4)
+				y=y-1;
+				x=x+1;
+				y=y+x;*/
