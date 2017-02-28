6066312f530057323c96bf2f28d6baeff04d7f16
BHIMNATH <you@example.com>
2017-02-28 23:11:19 +0530

    Just Read from user

---
+++ Useless.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.*;
+public class Useless{
+	public void hai(){
+		System.out.println("Hai");
+	}
+	public static void main(String[] args) {
+	/*Useless[] u=new Useless[3];
+	u[0]=new Useless();
+	u[1]=new Useless();
+	u[2]=new Useless();
+	for(int i = 0;i < u.length; i++){
+			u[i].hai();*/
+			int s;//"Hai";
+			Scanner key=new Scanner(System.in);
+			System.out.println("Enter Number");
+			s= key.nextInt();
+			System.out.println("You entered "+s);
+
+	}
+			
+}
